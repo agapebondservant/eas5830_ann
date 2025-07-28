45516a92a93685b186ed96f5baf39d34b6171aa1
agapebondservant <agapebondservant@yahoo.com>
2025-07-28 22:59:25 +0000

    updates

--- AMM.sol
+++ AMM.sol
@@ -65,13 +65,13 @@ contract AMM is AccessControl{
 		//YOUR CODE HERE 
 		if( sellToken == tokenA ){
 			ERC20(tokenA).transferFrom(msg.sender, address(this), sellAmount);
-			uint256 qtyAWithFee = qtyA * ((10000 - feebps)/10000);
+			uint256 qtyAWithFee = qtyA * (10000 - feebps)/10000;
 			swapAmt = qtyB - (invariant / (qtyAWithFee + sellAmount));
 			ERC20(tokenB).transfer(msg.sender, swapAmt);
 
 		} else {
 			ERC20(tokenB).transferFrom(msg.sender, address(this), sellAmount);
-			uint256 qtyBWithFee = qtyB * ((10000 - feebps)/10000);
+			uint256 qtyBWithFee = qtyB * (10000 - feebps)/10000;
 			swapAmt = qtyA - (invariant / (qtyBWithFee + sellAmount));
 			ERC20(tokenA).transfer(msg.sender, swapAmt);
 		}
