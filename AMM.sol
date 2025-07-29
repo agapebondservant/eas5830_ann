179d97d1229cc91818e5a294bd70b11c8597bbc0
agapebondservant <agapebondservant@yahoo.com>
2025-07-29 01:47:31 +0000

    updates

--- AMM.sol
+++ AMM.sol
@@ -66,7 +66,7 @@ contract AMM is AccessControl{
 		uint256 reserveInUpdated = ERC20(tokenIn).balanceOf(address(this));
 		uint256 reserveInActual = reserveInUpdated - reserveIn;
 		uint256 reserveInActualWithFee = reserveInActual * (1000 - feebps); // /1000;
-		uint256 reserveOut = reserveOut * reserveInActualWithFee;
+		reserveOut = reserveOut * reserveInActualWithFee;
 		reserveOut = reserveOut / (reserveIn * 1000 + reserveInActualWithFee);
 		//reserveOutput = reserveOut / (reserveIn + reserveInActualWithFee) * 1000;
 
